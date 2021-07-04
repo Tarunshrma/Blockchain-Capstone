ed8a88c30711a344d5b7ee1128ad64e4f533a360
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2021-07-04 19:49:49 +0530

    Added test cases fir SolnSquareVerifier

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,44 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+let SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+let Verifier = artifacts.require('verifier');
+let proof = require('./proof')
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('TestSolnSquareVerifier', accounts => {
+    const account_one = accounts[0]
+    const account_two = accounts[1]
+
+    beforeEach(async function () {
+        const SolutionVerifier = await Verifier.new({ from: account_one })
+        this.contract = await SolnSquareVerifier.new(SolutionVerifier.address, {
+            from: account_one
+        })
+    })
+
+    it('Test if a new solution can be added for contract', async function () {
+        let result = await this.contract.mintToken(account_two, 1,
+            proof.proof.a,
+            proof.proof.b,
+            proof.proof.c,
+            proof.inputs,
+            { from: account_one }
+        )
+
+        assert.equal(result.logs[0].event, 'SolutionAdded')
+    })
+
+    it('Test if an token can be minted for contract', async function () {
+        let mint = true
+        try {
+            await this.contract.mintToken(account_two, 1,
+                proof.proof.a,
+                proof.proof.b,
+                proof.proof.c,
+                proof.inputs,
+                { from: account_one }
+            )
+        } catch (e) {
+            mint = false
+        }
+
+        assert.equal(mint, true);
+    })
+})
